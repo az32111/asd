a1cc76a2b77e34d94f9997f4b77f21294d44f7e2
JJH <az32111@naver.com>
2019-09-17 21:30:39 +0900

    자바 소스 백업1

---
+++ src/com/company/Main.java
@@ -0,0 +1,114 @@
+package com.company;
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if (a < b) {
+            System.out.println("나중이 더 큼");
+        } else if (a == b) {
+            System.out.println("두 수가 같음");
+        } else {
+            System.out.println("처음이 더 큼");
+        }
+    }
+}
+
+//        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//        if (a % 2 == 0)
+//            System.out.println("짝수 입니다.");
+//        else
+//            System.out.println("음수 입니다.");
+//    }
+//}
+
+
+
+
+
+
+
+
+
+//    public static void main(String[] args) {
+//        // 입력값(숫자)을 받아서 입력한 숫자가 양수인지 음수인지 판단하여 양수면
+//        //"  입력한 값은 양수입니다" 라고 출력하고
+//        //음수일 경우 "입력한 값은 음수입니다"라고 출력하도록 알고리즘을 짜보세요
+//        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//        if (a == 0) {
+//            System.out.println("0 입니다.");
+//        } else if (a < 0) {
+//            System.out.println("음수 입니다.");
+//        } else {
+//            System.out.println("양수 입니다.");
+//        }
+//    }
+//}
+
+//    public static void main(String[] args) {
+//        int tvprogram = 99;
+//        if (tvprogram <= 15) {
+//            System.out.println("15세 관람가입니다");
+//        } else if (tvprogram<=16&&tvprogram>=19) {
+//            System.out.println("청소년 관람가 입니다");
+//        } else {
+//            System.out.println("미성년자관람불가");
+//        }
+//    }
+//}
+
+
+
+//>=
+//==
+//!= <= not
+//&& <= and
+//|| <= or
+
+//        int a = 10;
+//        int b = 7;
+//        if (a>b){
+//            System.out.println("a가 더 큽니다");
+//        }else{
+//            System.out.println("b가 더 큽니다");
+//        }
+//    }
+//}
+
+//    public static void main(String[] args) {
+////        // write your code here
+////        //조건문
+////        int val = 50;
+////        if (val < 10){
+////            System.out.println("성공");
+////        }else{
+////            System.out.println("실패");
+////        }
+//    public static void main(String[] args) {}
+//    // write your code here
+//    // 조건문
+//    int age = 19;
+//    if (age >=19){
+//        System.out.println("성인");}
+//    }else{
+//        System.out.println("미성년");}
+//}
+//        // write your code here
+//        // 조건문
+//        // int i =  15라고 가정했을때 값이 15일 경우에는 "성공"이라고 출력하고 값이 15가 아닐 경우에는
+//        //"실패"라고 출력되도록 코딩
+////        int i = 15;
+////        if (i == 15){
+////        System.out.println("실패");
+////    }else{
+////            System.out.println("성공");
+////        }
+
